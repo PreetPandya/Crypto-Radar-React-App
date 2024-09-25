dac228b93f0d2e1bf033d43829d06c489963f3ff
Preet Pandya <68130119+PreetPandya@users.noreply.github.com>
2024-09-26 00:51:38 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,12 @@ const Header = () => {
     <>
       <Row className="my-5">
         <Col md={12} sm={12}>
-          <h1 id="header-title">Crypto Radar</h1>
+          <h1 className="header-title">
+            <span>Crypto</span>
+            <span style={{ color: "#EEBC1D" }}>Radar</span>
+          </h1>
         </Col>
+
         <Col md={12} sm={12}>
           <p id="header-tagline">
             "Crypto Radar: Your Ultimate Guide to Real-Time Crypto Insights"
